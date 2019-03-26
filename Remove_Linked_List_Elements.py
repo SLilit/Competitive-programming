154edc655b367bed2465b536450dd82ab21406ed
LILIT <lilit555ai@gmail.com>
2019-03-26 00:48:12 -0700

    Add another solution to Remove_Linked_List_Element

--- Remove_Linked_List_Elements.py
+++ Remove_Linked_List_Elements.py
@@ -32,3 +32,20 @@ class Solution:
 
 
 
+        if not head:
+            return head
+        pre = None
+        curr = head
+        
+        while curr:
+            if curr.val == val:
+                if pre:
+                    pre.next = curr.next
+                    curr = curr.next
+                else:
+                    curr = curr.next
+                    head = curr
+            else:
+                pre, curr = curr, curr.next
+         
+        return head
