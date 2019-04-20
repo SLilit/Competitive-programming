85dc100c9e05a6591ae89cf8b522ddc1ee9861e0
LILIT <lilit555ai@gmail.com>
2019-04-20 00:31:27 -0700

    Add intersection-of-two-arrays.py

---
+++ intersection-of-two-arrays.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        def inter(A,B):
+            
+            result = []
+            for a in A:
+                if a in B:
+                    result.append(a)
+            return result
+        n1 = set(nums1)
+        n2 = set(nums2)
+        if len(n1) > len(n2):
+            return inter(n1, n2)
+        return inter(n2, n1)
+
+
+
+        
