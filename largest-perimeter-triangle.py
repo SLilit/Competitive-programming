2b74b48062d6592dfa363e41ec6f7f39549d7d0c
LILIT <lilit555ai@gmail.com>
2019-04-25 11:57:23 -0700

    Add another solution to largest-perimeter-triangle.py

--- largest-perimeter-triangle.py
+++ largest-perimeter-triangle.py
@@ -8,5 +8,16 @@ class Solution:
         return 0
         
 
-        
+        if len(A) < 3:
+            return 0
+       
+        A.sort()
+        A = A[::-1]
+
+        for i in range(len(A)):
+
+            if i+2 >= len(A):
+                return 0
+            if A[i] < A[i+1] + A[i+2]:
+                return sum(A[i:i+3])
             
