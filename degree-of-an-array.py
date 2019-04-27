c2e8f6cc4ddb58ef225ca9a55a06f7e7567068df
LILIT <lilit555ai@gmail.com>
2019-04-27 14:48:11 -0700

    Remove right dict in degree-of-an-array.py

--- degree-of-an-array.py
+++ degree-of-an-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def findShortestSubArray(self, nums: List[int]) -> int:
         count = {}
-        right = {}
         left = {}
         max_d = 0
         min_l = float("inf")
@@ -9,17 +8,14 @@ class Solution:
             n = nums[i]
             if n not in count:
                 count[n] = 1
-                right[n] = left[n] = i
+                left[n] = i
             else:
                 count[n] += 1
-                right[n] = i
             if count[n] > max_d:
-                #print('1: ', count[n], right[n], left[n])
                 max_d = count[n]
-                min_l = right[n] - left[n] + 1
-            elif count[n] == max_d and (right[n] - left[n] + 1) < min_l :
-                #print('2: ', count[n], right[n], left[n])
-                min_l = right[n] - left[n] + 1
+                min_l = i - left[n] + 1
+            elif count[n] == max_d:
+                min_l = min(min_l,i - left[n] + 1)
             
         return min_l
                 
